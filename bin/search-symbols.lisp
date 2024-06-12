eef77b0560cb03bb4df6342344d50b7fdf19d8e9
Anders Hoff <inconvergent@gmail.com>
2024-06-13 00:05:48 +0200

    tweaks to search symbols script

--- bin/search-symbols.lisp
+++ bin/search-symbols.lisp
@@ -2,11 +2,11 @@
 
 (let ((q (second (cmd-args))) (pkg (third (cmd-args))))
  (qry (ls "~/common-lisp/*/src/packages.lisp")
-      dat-read-file _@flatn* second                        ; package names
-      [(or (not pkg) (isub? s@_ pkg))] extsym? _@flatn*    ; filter packages
-      [(or (string= s@q "_") (isub? s@_ q))]               ; filter symbol
+      dat-read-file _@flatn* second                     ; package names
+      [(or (not pkg) (isub? s@_ pkg))] extsym? _@flatn* ; filter packages
+      [(or (string= s@q "_") (isub? s@_ q))]            ; filter symbol ; _ matches all
       #((join (head (splt (stdstr _@describe)
-                          #.(str! #\Newline) nil) -1)      ; some formatting
-               #\Newline " ; ")
-        (out "~&████████████████████████████████████████~& ; ~a~&~%" _))))
+                          #.(str! #\Newline) nil) -1)   ; some formatting
+               #\Newline ";; ")
+        (out "~&~&██ ~a~&~%" _))))
 
